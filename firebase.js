eb7af8777a620f0eb0998c632b5b815a16a52dbb
ludodigitalmkt-prog <ludodigitalmkt@gmail.com>
2026-04-03 16:07:32 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBCFIL5NCeutlSciYuHj8sV6fr88-T2d-w",
@@ -11,15 +10,8 @@ const firebaseConfig = {
   appId: "1:173305323322:web:b2cd9947aaa9806dc33a03"
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Inicializa Banco de Dados com Cache Offline Ativado
-const db = initializeFirestore(app, {
+// Exportando o db para o app.js usar
+export const db = initializeFirestore(app, {
   localCache: persistentLocalCache({tabManager: persistentMultipleTabManager()})
 });
-
-console.log("🔥 Firebase Inicializado com Sucesso!");
-
-export { app, auth, db };
