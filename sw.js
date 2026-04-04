755263ccf6ebb0dca3bc12d15d1833c18cce70fb
ludodigitalmkt-prog <ludodigitalmkt@gmail.com>
2026-04-04 15:02:05 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,18 @@
-const CACHE_NAME = "ludomkt-v2";
-const assets = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./firebase.js",
-  "./logo.png"
-];
+const CACHE_NAME = 'ludomkt-v2';
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+// Instala o motor do App
+self.addEventListener('install', (event) => {
+    console.log('[LudoMKT App] Instalado com sucesso.');
+    self.skipWaiting();
 });
 
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(caches.match(fetchEvent.request).then(res => res || fetch(fetchEvent.request)));
+// Ativa o motor do App
+self.addEventListener('activate', (event) => {
+    console.log('[LudoMKT App] Ativado.');
+    event.waitUntil(clients.claim());
+});
+
+// Permite requisições de rede normais
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request).catch(() => console.log("Você está offline.")));
 });
